53a3b7243ad591c62b07fedf9697a21505cf1c10
TelamonPegasus <telamon025115@gmail.com>
2022-02-15 06:33:07 -0500

    set up infrastructure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThirdwebProvider } from "@3rdweb/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const supportedChainIds = [1, 3, 4, 42, 80001];
+  // Ethereun Mainnet - 1 (ETH)
+  // Ropsten Test Network - 3 (ETH)
+  // Rinkeby Test Network - 4 (ETH)
+  // Kovan Test Network -  42 (ETH)
+  // Polygon Mumbai Test Network - 80001 (MATIC)
+
+  const connectors = {
+    injected: {},
+    // magic: {
+    //   apiKey: 'pk_...', // Your magic api key
+    //   chainId: 1, // The chain ID you want to allow on magic
+    // },
+    // walletconnect: {},
+    // walletlink: {
+    //   appName: 'web3-auth',
+    //   url: 'http://localhost:3000',
+    //   darkMode: false,
+    // },
+  };
+
+  return (
+    <ThirdwebProvider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
